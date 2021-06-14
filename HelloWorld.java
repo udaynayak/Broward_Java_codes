fb4bfd859046a85a1bf9415592c38dcd4a8bab5b
udaynayak <uday_nayak@hotmail.com>
2021-06-14 14:53:42 -0400

    First File - testing

---
+++ HelloWorld.java
@@ -0,0 +1,34 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		// This is comment line
+		// These are the simple print statements
+		
+		System.out.println("Hello World");	
+		System.out.println("Programming is fun");	
+		
+		System.out.println("Hello World" +  " I am here");	
+		
+		System.out.println(10 +10);
+		
+		//Variables   (input)
+		int num1 = 10;
+		int num2 = 20;
+	 
+		
+		//Process
+		int total = num1 + num2;
+		
+		//Output
+		System.out.println("Total is " + total);		
+		
+		
+
+	}
+
+}
+
+
